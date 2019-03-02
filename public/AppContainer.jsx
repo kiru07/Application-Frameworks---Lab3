ee5f4da1dc24d6267e9c7689a4a1dc6e147463b6
kiru07 <kirushanthan007@gmail.com>
2019-03-02 13:05:57 +0530

    Part11: Updated AppContainer

--- public/AppContainer.jsx
+++ public/AppContainer.jsx
@@ -2,14 +2,23 @@
 
 import React, { Component } from 'react';
 
+import Users from './Users';
+
 export default class AppContainer extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            users: [{
+                id: Date.now(),
+                name: 'John'
+            }]
+        }
     }
 
     render() {
         return <div>
-            <h2>Hello World</h2>
+            <h2>Users App</h2>
+            <Users users = {this.state.users} />
         </div>;
     }
 }
