fb7be95da1824fa48ba87cc376f7841782a1a753
kiru07 <kirushanthan007@gmail.com>
2019-03-02 12:43:03 +0530

    Upto part 8

---
+++ public/AppContainer.jsx
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, { Component } from 'react';
+
+export default class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+            <h2>Hello World</h2>
+        </div>;
+    }
+}
